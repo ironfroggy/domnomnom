b5c1d2de89ef4bd36e5b00bf8025e302dd977855
Calvin Spealman <ironfroggy@gmail.com>
2011-02-27 20:22:21 -0500

    String or object as data

--- domnomnom.js
+++ domnomnom.js
@@ -7,22 +7,11 @@ $.fn.domnomnom = function domnomnom(data) {
     $(this).each(function(){
         var el = $(this);
         if (typeof data === "string") {
-            el.html(data);
+            el.text(data);
         } else {
             $.each(data, function(key, value) {
-                console.info(el, key, value);
-                if (typeof value === "string") {
-                    el.find(key).html(value);
-                } else {
-                    $.each(value, function(key, value) {
-                        var slot = $(key);
-                        if (slot.hasClass('clone-after')) {
-                            console.log("clone-after", slot, value, slot.parent())
-                            slot.clone().domnomnom(value).appendTo(slot.parent());
-                        }
-                    })
-                }
-            });
+                el.find(key).domnomnom(value);
+            })
         }
     });
 
