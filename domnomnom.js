1872234796bc2ff5b70fefc87dd98559a85f865a
Calvin Spealman <calvin@pantechnoco.com>
2011-02-27 19:35:43 -0500

    more

--- domnomnom.js
+++ domnomnom.js
@@ -6,10 +6,24 @@ $.fn.domnomnom = function domnomnom(data) {
 
     $(this).each(function(){
         var el = $(this);
-        $.each(data, function(key, value) {
-            console.info(el, key, value);
-            el.find(key).html(value);
-        });
+        if (typeof data === "string") {
+            el.html(data);
+        } else {
+            $.each(data, function(key, value) {
+                console.info(el, key, value);
+                if (typeof value === "string") {
+                    el.find(key).html(value);
+                } else {
+                    $.each(value, function(key, value) {
+                        var slot = $(key);
+                        if (slot.hasClass('clone-after')) {
+                            console.log("clone-after", slot, value, slot.parent())
+                            slot.clone().domnomnom(value).appendTo(slot.parent());
+                        }
+                    })
+                }
+            });
+        }
     });
 
     return result;
