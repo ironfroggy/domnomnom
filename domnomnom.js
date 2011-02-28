78639fcbf0ca3e7eb0e0b1bb973717512edca676
Calvin Spealman <ironfroggy@gmail.com>
2011-02-27 20:36:16 -0500

    Hide clone-after slots and add array handling.

--- domnomnom.js
+++ domnomnom.js
@@ -8,8 +8,14 @@ $.fn.domnomnom = function domnomnom(data) {
         var template = $(this)
         ,   copy = template.clone()
         ;
+
         if (typeof data === "string") {
             copy.text(data);
+        } else if ($.isArray(data)) {
+            $.each(data, function(i, value) {
+                template.domnomnom(value);
+            })
+            return;
         } else {
             $.each(data, function(key, value) {
                 copy.find(key).domnomnom(value);
@@ -17,6 +23,8 @@ $.fn.domnomnom = function domnomnom(data) {
         }
 
         if (template.hasClass('clone-after')) {
+            copy.removeClass('clone-after');
+            console.debug(copy);
             template.parent().append(copy);
         } else {
             template.replaceWith(copy);
