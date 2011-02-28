b59802348cc39b3a0610086c37e48ad0d29f8b79
Calvin Spealman <ironfroggy@gmail.com>
2011-02-27 21:00:54 -0500

    Remove unused result collection

--- domnomnom.js
+++ domnomnom.js
@@ -1,10 +1,6 @@
 (function() {
 
     $.fn.domnomnom = function domnomnom(data) {
-        var result = $();
-        function add_result(r) {
-            result = result.add(r);
-        }
 
         $(this).each(function(){
             var template = $(this)
