83abcb9bc217761953af5ec52c8ab720a6af7413
Calvin Spealman <ironfroggy@gmail.com>
2011-02-27 20:48:23 -0500

    Add deafult clone-after for LI and TR. Also, operate better on some data.

--- domnomnom.js
+++ domnomnom.js
@@ -1,35 +1,44 @@
-$.fn.domnomnom = function domnomnom(data) {
-    var result = $();
-    function add_result(r) {
-        result = result.add(r);
+(function() {
+
+    var default_applies = {
+            LI: "clone-after"
+        ,   TR: "clone-after"
     }
+    ;
 
-    $(this).each(function(){
-        var template = $(this)
-        ,   copy = template.clone()
-        ;
-
-        if (typeof data === "string") {
-            copy.text(data);
-        } else if ($.isArray(data)) {
-            $.each(data, function(i, value) {
-                template.domnomnom(value);
-            })
-            return;
-        } else {
-            $.each(data, function(key, value) {
-                copy.find(key).domnomnom(value);
-            })
+    $.fn.domnomnom = function domnomnom(data) {
+        var result = $();
+        function add_result(r) {
+            result = result.add(r);
         }
 
-        if (template.hasClass('clone-after')) {
-            copy.removeClass('clone-after');
-            console.debug(copy);
-            template.parent().append(copy);
-        } else {
-            template.replaceWith(copy);
-        }
-    });
+        $(this).each(function(){
+            var template = $(this)
+            ,   copy = template.clone()
+            ;
 
-    return result;
-};
+            if (typeof data === "string") {
+                copy.text(data);
+            } else if ($.isArray(data)) {
+                $.each(data, function(i, value) {
+                    template.domnomnom(value);
+                })
+                return;
+            } else {
+                $.each(data, function(key, value) {
+                    copy.find(key).domnomnom(value);
+                })
+            }
+
+            if (template.hasClass('clone-after') || default_applies[template[0].tagName] == "clone-after") {
+                copy.removeClass('clone-after');
+                template.parent().append(copy);
+            } else {
+                template.replaceWith(copy);
+            }
+        });
+
+        return result;
+    };
+
+})();
