56bd259b50757bdd8b7e84f4ce5d678c0fe1ee04
Calvin Spealman <calvin@caktusgroup.com>
2012-11-20 08:06:48 -0500

    Several changes made to integrate into an unrelated project:     - Pulled template extraction out of invocation, done at page load now     - Template extraction has also been improved to work in more complex       conditions     - applyinner rule to map data inside templates     - Date parsing from data when template type=date     - Templates can specify a function in the data to invoke     - Simplified application of nested data inside template trees

--- domnomnom.js
+++ domnomnom.js
@@ -1,5 +1,24 @@
 (function() {
 
+    $(function(){
+        // Look for template usages and load templates
+        $(this).each(function(){
+            $(this).find('[data-template]').each(function(){
+                var template = $(this);
+                var match = template.data('template');
+                var source = [];
+                var P = template;
+                while (source.length === 0 && P[0] !== document) {
+                    P = P.parent();
+                    source = P.find(match);
+                }
+                template.html(source.eq(0).html());
+                template.data(source.eq(0).data());
+                template.removeAttr('data-template');
+            });
+        });
+    });
+
     $.fn.domnomnom = function domnomnom(data) {
 
         // TODO: Figure out what this was supposed to be
@@ -9,74 +28,84 @@
             return $(this).domnomnom(data_obj);
         }
 
-        // Look for template usages and load templates
-        $(this).each(function(){
-            $(this).find('[data-template]').each(function(){
-                var template = $(this);
-                template.html(template.siblings(template.attr('data-template')).html());
-                template.removeAttr('data-template');
-            });
-        });
-
         $(this).each(function(){
             // For each element matched, populate with data
             var template = $(this)
             ,   copy = template.clone()
+            ,   applyinner = template.data('applyinner')
             ;
 
-            // Strings set textnode directly
-            if (typeof data === "string") {
-                copy.text(data);
-            // Arrays act as a sequence of calls with each item
-            } else if ($.isArray(data)) {
-                $.each(data, function(i, value) {
-                    template.domnomnom(value);
-                })
-                return;
-            // Objects match inner nodes to other data
+            if (!!applyinner) {
+                template.find(applyinner).domnomnom(data);
             } else {
-                console.log(copy, data);
-                $.each(data, function(key, value) {
-                    // This is where renames can happen.
-                    var ref = copy.attr('data-rename-' + key);
-                    copy.removeAttr('data-rename-' + key);
-                    if (ref) {
-                        key = ref;
-                    }
 
-                    // selector/attr is used to target attributes directly
-                    if (key.indexOf('/') >= 0) {
-                        var _ = key.split('/')
-                        ,   key = _[0]
-                        ,   attribute = _[1]
-                        ,   el = copy.find(key)
-                        ;
+                var datatype = template.data('type');
+                if (datatype == 'date') {
+                    var ts = Date.parse(data);
+                    data = new Date(ts);
+                }
 
-                        if (attribute) {
-                            el.attr(attribute, value);
-                        } else {
-                            $.each(value, function(attribute, value) {
-                                el.attr(attribute, value);   
-                            });
+                // If the template defines a method, call it
+                var call = template.data('call')
+                if (call) {
+                    data = data[call].call(data);
+                }
+
+                // Strings set textnode directly
+                if (typeof data === "string") {
+                    copy.text(data);
+                // Arrays act as a sequence of calls with each item
+                } else if ($.isArray(data)) {
+                    $.each(data, function(i, value) {
+                        template.domnomnom(value);
+                    })
+                    return;
+                // Objects match inner nodes to other data
+                } else {
+                    $.each(data, function(key, value) {
+
+                        // This is where renames can happen.
+                        var ref = copy.data('rename-' + key);
+                        copy.removeAttr('data-rename-' + key);
+                        if (ref) {
+                            key = ref;
                         }
-                    // Finally, find the child and fill with data
-                    } else {
-                        copy.find(key).domnomnom(value);
-                    }
-                })
-            }
 
-            if (template.attr('data-clone') == ':after') {
-                template.parent().append(copy);
-            } else if (template.attr('data-clone') == ':before') {
-                copy.removeClass('clone-before');
-                template.parent().prepend(copy);
-            } else { // implies data-clone=replace
-                template.replaceWith(copy);
-            }
+                        // selector/attr is used to target attributes directly
+                        if (key.indexOf('/') >= 0) {
+                            var _ = key.split('/')
+                            ,   key = _[0]
+                            ,   attribute = _[1]
+                            ,   el = copy.find(key)
+                            ;
 
-            copy.removeAttr('data-clone');
-            copy.removeClass('slot');
+                            if (attribute) {
+                                el.attr(attribute, value);
+                            } else {
+                                $.each(value, function(attribute, value) {
+                                    el.attr(attribute, value);   
+                                });
+                            }
+                        // Finally, find the child and fill with data
+                        } else {
+                            copy.find(key).domnomnom(value);
+                        }
+                    })
+                }
+
+                if (template.data('clone') == ':after') {
+                    //template.parent().append(copy);
+                    template.after(copy);
+                } else if (template.data('clone') == ':before') {
+                    copy.removeClass('clone-before');
+                    template.before(copy);
+                } else { // implies data-clone=replace
+                    template.replaceWith(copy);
+                }
+
+                copy.removeAttr('data-clone');
+                copy.removeClass('slot');
+            }
         });
 
         return $(this);
